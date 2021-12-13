dc52f7b9164e09cd427ec003a794c44ff256cbd5
natibiruk08 <natnaelbiruk3@gmail.com>
2021-12-13 13:12:40 +0300

    login and signup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductsList from "./pages/ProductsList";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div>
-      <Product />
+      <Login />
     </div>
   );
 }
