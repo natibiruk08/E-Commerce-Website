c5d820f71ceccc9eaf9619b793019280e838fda5
natibiruk08 <natnaelbiruk3@gmail.com>
2021-12-13 12:45:31 +0300

    added single product page(product details)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Home from "./pages/Home";
+import Product from "./pages/Product";
 import ProductsList from "./pages/ProductsList";
 
 function App() {
   return (
     <div>
-      <ProductsList />
+      <Product />
     </div>
   );
 }
